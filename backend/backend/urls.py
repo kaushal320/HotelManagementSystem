84301cc2832bf67f471ac3f9c053b16dee8aa54f
kaushal320 <nepalkaushal69@gmail.com>
2024-12-11 19:05:28 +0545

    fetching data

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -22,4 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
      path("api/", include('website.urls')),
+      path("api/", include('rooms.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- backend/rooms/views.py
+++ backend/rooms/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from rest_framework.generics import ListAPIView
 # Create your views here.
+from . import serializers
+from . import models
+
+class RoomTypeListView(ListAPIView):
+    serializer_class=serializers.RoomTypeSerializer
+    queryset=models.RoomType.objects.all()
+
